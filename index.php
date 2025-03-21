dfa88445f79549de497684554011250d31bb0eee
dany57ru <127332379+dany57ru@users.noreply.github.com>
2025-03-21 15:12:56 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,117 @@
+
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Арнаутов Данил 11 МО</title>
+    <style>
+        #conteiner{
+            width: 1400px;
+            height: 945px;
+            margin: 0 auto;
+        }
+        body{
+            /*background: linear-gradient(0deg, #FEC568 0%, #E3FFA1 100%);*/
+            background-image:url("16983840_mancha_azul.jpg");
+            background-size: 100%;
+
+        }
+        
+
+        #left{
+            width: 700px;
+            float: left;
+            min-height: 900px;
+            margin-right: 50px;
+            margin-left: 60px;
+            
+        }
+        #right{
+            width: 700px;
+            min-height: 900px;
+            margin-left: 50%;
+            
+        }
+        ol {
+            list-style-type: circle;
+            color:#660066;
+            
+        }
+        h1{
+            margin: auto 0;
+            text-align: center;
+            color:#660066 ;
+        }
+        h2{
+            color: #993399 ;
+        }
+             
+    </style>
+
+</head>
+<body>
+    <div id = "conteiner">
+    <h1 > Лабораторные работы по PHP</h1>
+    <div id = "left">
+    <h2>1. Знакомство с PHP </h2>
+    <ol>
+        <li> <a href="http://f1097100.xsph.ru/MyProject/users.php">Классы и интерфейсы</a></li>
+        <!-- <li><a href="http://f0868605.xsph.ru/lab_1/vars.php">Работа с переменными и типами</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_1/if.php">Работа с конструкцией if-elseif-else</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_1/switch.php">Работа с конструкцией switch</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_1/match.php"> Работа с конструкцией match</li>
+        <li><a href="http://f0868605.xsph.ru/lab_1/array.php">Работа с массивами</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_1/constant.php">Работа с константами</a></li> -->
+    </ol>  
+    <!-- <h2>2. Циклы и пользовательские функции </h2>
+    <ol>
+        <li> <a href="http://f0868605.xsph.ru/lab_2/for.php">Работа с циклом for </a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_2/while.php">Работа с циклом while</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_2/table.php">Практическое использование циклов</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_2/menu.php">Практическое использование цикла foreach</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_2/gettable.php">Работа с функциями</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_2/getmenu.php">Практическое использование функций</a></li>
+    </ol>
+    <h2>3. Встроенные функции</h2>
+    <ol>
+        <li> <a href="http://f0868605.xsph.ru/lab_3/strings.php">Использование функций обработки строк</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_3/date.php">Использование функций даты и времени</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_3/constants.php">Использование встроенных функций(константы)</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_3/functions.php">Использование встроенных функций(функции)</a></li>
+        <li><a href="http://f0868605.xsph.ru/lab_3//inc/index.php">Подключение файлов</a></li>
+    </ol>
+     <h2>4. Обработка форм</h2>
+     <ol>
+         <li> <a href="http://f0868605.xsph.ru/lab_4/index.php"> Метод GET и обработка запроса </a></li>
+         <li><a href="http://f0868605.xsph.ru/lab_4/index.php?id=table"> Метод POST и обработка запроса </a></li>
+         <li><a href="http://f0868605.xsph.ru/lab_4/index.php?id=calc">Создание он-лайн калькулятора</a></li>
+     </ol>
+    </div>
+    <div id = "right">
+     <h2>5. Работа с файлами. Загрузка файлов на сервер</h2>
+     <ol>
+         <li> <a href="http://f0868605.xsph.ru/lab_5/file.php"> Работа с файлами </a></li>
+         <li><a href="http://f0868605.xsph.ru/lab_5/upload/upload.php"> Загрузка файлов на сервер </a></li>
+         <li><a href="http://f0868605.xsph.ru/lab_4/index.php?id=contact"> Отправка e-mail </a></li>
+     </ol>
+     <h2>6. Работа с протоколом HTTP: cookie, заголовки ответа сервера. Сессии</h2>
+     <ol>
+         <li> <a href="http://f0868605.xsph.ru/lab_6/cookie.php"> Использование cookie </a></li>
+         <li><a href="http://f0868605.xsph.ru/lab_6/session/page1.php">Использование сессий</a></li>
+     </ol>
+     <h2>7. Объектно-ориентированное программирование </h2>
+     <ol>
+         <li> <a href="http://f0868605.xsph.ru/lab_7/users.php">Использование конструктора и деструктора </a></li>
+         <li> <a href="http://f0868605.xsph.ru/lab_7/super_users.php"> Реализация наследования классов  </a></li>
+     </ol>
+     <h2>8. Использование сервера баз данных MySQL в приложениях PHP </h2>
+     <ol>
+         <li> <a href="http://f0868605.xsph.ru/lab_8/gbook_new.php">Работа с базой данных </a></li>
+     </ol> -->
+    </div>
+</div>
+</body>
+</html>
+
+<!--?>-->
