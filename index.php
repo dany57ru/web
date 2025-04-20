63077c954d5d42b0f135047396a49af672499a27
dany57ru <127332379+dany57ru@users.noreply.github.com>
2025-04-20 22:01:14 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,122 @@
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Арнаутов Данил 11 МО</title>
+    <style>
+        #conteiner{
+            width: 1400px;
+            height: 945px;
+            margin: 0 auto;
+        }
+        body{
+            /*background: linear-gradient(0deg, #FEC568 0%, #E3FFA1 100%);*/
+            background-image:url("c39ffd269183d4783c43d64c40adb06e.jpg");
+            background-size: 100%;
+
+        }
+        
+
+        #left{
+            width: 700px;
+            float: left;
+            min-height: 900px;
+            margin-right: 50px;
+            margin-left: 60px;
+            
+        }
+        #right{
+            width: 700px;
+            min-height: 900px;
+            margin-left: 50%;
+            
+        }
+        ol {
+            list-style-type: circle;
+            color:#111111;
+            
+        }
+        h1{
+            margin: auto 0;
+            text-align: center;
+            color:#111111 ;
+        }
+        h2{
+            color: #111111 ;
+        }
+             
+    </style>
+
+</head>
+<body>
+    <div id = "conteiner">
+    <h1 > Лабораторные работы</h1>
+    <div id="left">
+    <div id = "left">
+    <h2>1. Классы и интерфейсы </h2>
+    <ol>
+        <li> <a href="http://e90048lb.beget.tech/MyProject/users.php">Классы и интерфейсы</a></li>
+        <li> <a href="http://e90048lb.beget.tech/MyProject/uml.html">UML-диаграмма</a></li>
+    </ol> 
+    <h2>2. Паттерны проектирования</h2>
+    <ol>
+        <li> Порождающие паттерны 
+        <ol> 
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/FactoryMethod/Conceptual/index.php">Фабричный метод</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/AbstractFactory/Conceptual/index.php">Абстрактная фабрика</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Builder/Conceptual/index.php">Строитель</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Prototype/Conceptual/index.php">Прототип </a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Singleton/Conceptual/index.php">Одиночка </a> </li>
+        </ol>
+        </li>
+        <li> Структурные паттерны
+        <ol> 
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Adapter/Conceptual/index.php">Адаптер</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Bridge/Conceptual/index.php">Мост</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Composite/Conceptual/index.php">Компоновщик</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Decorator/Conceptual/index.php">Декоратор</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Facade/Conceptual/index.php">Фасад</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Flyweight/Conceptual/index.php">Легковес</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Proxy/Conceptual/index.php">Заместитель</a> </li>
+        </ol>
+        </li>
+        <li> Поведенческие паттерны
+        <ol> 
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/ChainOfResponsibility/Conceptual/index.php">Цепочка обязанностей</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Command/Conceptual/index.php">Команда</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Iterator/Conceptual/index.php">Итератор</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Mediator/Conceptual/index.php">Посредник</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Memento/Conceptual/index.php">Снимок</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Observer/Conceptual/index.php">Наблюдатель</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/State/Conceptual/index.php">Состояние</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/Strategy/Conceptual/index.php">Стратегия</a> </li>
+        <li> <a href="http://e90048lb.beget.tech/lab2/src/RefactoringGuru/TemplateMethod/Conceptual/index.php">Шаблонный метод</a> </li>
+        </ol>
+        </li>
+    </ol>
+    <h2>3. Паттерн Модель-Представление-Контроллер</h2>
+    <ol>
+         <li> <a href="http://e90048lb.beget.tech/lab3/patterns/singleton/settings_use.php">Одиночка (Singleton)</a></li>
+         <li> <a href="http://e90048lb.beget.tech/lab3/patterns/factory-method/factory_use.php">Фабричный метод (Factory Method)</a></li>
+         <li> <a href="http://e90048lb.beget.tech/lab3/patterns/factory-method/factory-method.html">UML-диаграмма factory_method</a></li>
+         <li> <a href="http://e90048lb.beget.tech/lab3/patterns/mvc/mvc_use.php">Модель-Представление-Контроллер</a></li>
+         <li> <a href="http://e90048lb.beget.tech/lab3/patterns/mvc/mvc.html">UML-диаграмма MVC</a></li>
+    </ol> 
+    </div>
+    <div id="right">
+    <h2>4. Использование ООП с базой данных SQLite</h2>
+    <ol>
+         <li> <a href="http://e90048lb.beget.tech/lab4/news.php">Новости</a></li>
+         <li> <a href="http://e90048lb.beget.tech/lab4/news.html">UML-диаграмма News</a></li>
+    </ol>
+    <h2>5. Standard PHP Library</h2>
+    <ol>
+         <li> <a href="http://e90048lb.beget.tech/lab5/news.php">Новости 2</a></li>
+         <li> <a href="http://e90048lb.beget.tech/lab5/numeric.html">UML-диаграмма NumbersSquared </a></li>
+    </ol> 
+    </div>
+    </div>
+    </div>
+</body>
+</html>
